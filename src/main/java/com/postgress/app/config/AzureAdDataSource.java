f7c29c63b5395069afd7bb0a1ce805e379bcf5c3
virendra <vspms1983@gmail.com>
2022-03-15 22:02:58 +0530

    change user name

--- src/main/java/com/postgress/app/config/AzureAdDataSource.java
+++ src/main/java/com/postgress/app/config/AzureAdDataSource.java
@@ -9,7 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "spring.datasource.hikari")
+@ConfigurationProperties(prefix = "spring.datasource")
 public class AzureAdDataSource extends HikariDataSource {
 
     private final SimpleTokenCache cache;
