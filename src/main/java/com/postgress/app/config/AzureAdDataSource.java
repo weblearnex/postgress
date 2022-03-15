81a219a5d76b74f5a32d336fca131a00403587fa
virendra <vspms1983@gmail.com>
2022-03-15 22:46:17 +0530

    change user name

--- src/main/java/com/postgress/app/config/AzureAdDataSource.java
+++ src/main/java/com/postgress/app/config/AzureAdDataSource.java
@@ -5,13 +5,17 @@ import com.azure.core.credential.SimpleTokenCache;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.credential.TokenRequestContext;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
+
 @ConfigurationProperties(prefix = "spring.datasource.hikari")
 public class AzureAdDataSource extends HikariDataSource {
-
+    Logger logger = (Logger) LoggerFactory.getLogger(AzureAdDataSource.class);
     private final SimpleTokenCache cache;
 
     public AzureAdDataSource(TokenCredential credential) {
@@ -25,11 +29,12 @@ public class AzureAdDataSource extends HikariDataSource {
                 .retry(1L)
                 .blockOptional()
                 .orElseThrow(() -> new RuntimeException("Attempt to retrieve AAD token failed"));
-
+        logger.info(accessToken.getToken());
         return accessToken.getToken();
     }
 
     private static TokenRequestContext createRequestContext() {
+
         return new TokenRequestContext().addScopes("https://ossrdbms-aad.database.windows.net");
     }
 }
